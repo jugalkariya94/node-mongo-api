8af9aae8ee9cd72203ecf209a4a7a17fd16dd03b
jugalkariya94 <jugalkariya@gmail.com>
2019-11-03 18:46:54 +0530

    add a model and simple get and save operation

--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,46 @@
+// a sample route which save and get the post
+
 // get router and define routes and process there
 var router = require('express').Router();
+var Post = require('../models/post');
 
 // @method GET
-// a simple get route
-router.get('/', function (req, res) {
+// @desc get all posts
+router.get('/', async function (req, res) {
+	let posts = await Post.find();
 	res.json({
 		success: true,
-		message: 'get requested for post'
+		posts: posts
 	});
+});
+
+// @method GET
+// @desc get post based on id
+router.get('/:id', async function (req, res) {
+	let post = await Post.findById(req.params.id);
+	res.json({
+		success: true,
+		post: post
+	});
+});
+
+// @method GET
+// @desc save a new post
+router.post('/save', function (req, res) {
+	let{ title, description} = req.body;
+	let newPost = new Post({
+		title: title,
+		description: description
+	});
+	newPost.save((err, result) => {
+		if (err) {
+			console.log(err);
+			res.send(JSON.stringify({status: "error", value: "Error, db request failed"}));
+			return
+		}
+		res.status(201).send(JSON.stringify({status: "ok", post: result}));
+	});
+
 });
 
 // export defined routes
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // Setup of the app
 var express = require('express');
 var app = express();
-var config = require('./config');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var config = require('./config');
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 
 // register routes
 require('./routes')(app);
